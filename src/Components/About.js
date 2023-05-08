a5e8d89cd8b3e6732493266eb29f4c00e2aea918
Larry Motuzis <mlauris2006@yahoo.com>
2023-05-08 13:55:49 -0500

    added bioImages folder with images

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,9 +2,25 @@ import React from "react";
 
 const About = () => {
     return (
-        <div>
-            <h2> About Page </h2>
-            <div></div>
+        <div className="flex flex-col w-5/6 mx-auto">
+            <div className="flex flex-col">
+             <div className="w-40 border border-black place-self-center mt-2 mb-1"></div>
+             <h2 className="text-3xl font-bold text-center whitespace-nowrap"> Our People </h2>
+             <div className="w-40 border border-black place-self-center mt-2 mb-1"></div>
+            </div>
+            <div>
+                <p> <span className="font-bold"> Michael L. Cooper, MAI </span> is the founder and President of the Cooper Valuation Group, which he started in 2010.
+                    Mr. Cooper’s core competencies include numerous assignments involving age-restricted housing, assisted living and nursing facilities throughout Illinois, Iowa, Wisconsin, Indiana, Ohio, Missouri, and Michigan. His experience includes a broad range of operations ranging from private pay to Medicaid/Medicare facilities for projects operating successfully as well as work out deals.
+                    He is familiar with <span className="font-bold"> HUD’s Lean Program </span>for insuring mortgages for Section 232’s (nursing and assisted living facilities). HUD developed the LEAN process in 2008 specifically for Section 232 applications.</p>
+            </div>
+            <div className="flex flex-row">
+                <div className="border border-black">
+                    
+                </div>
+                <div> 
+                    Another guy 
+                </div>
+            </div>
         </div>
     )
 };
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,14 +6,14 @@ const Home = () => {
         <div className="flex flex-col w-5/6 mx-auto">
             <h2 className="text-3xl font-bold text-center"> Home Page </h2>
             <div className="w-36 border border-black place-self-center"></div>
-            <div className="w-5/6 self-center mt-4">
+            <div className="self-center mt-4">
                 <h2 className="text-2xl" > The <span className="font-bold">Cooper Valuation Group</span> is a commercial real estate appraisal and market analysis firm specializing in healthcare and senior housing properties.</h2>
             </div>
             <div className="w-3/6 border border-black place-self-center mt-4 mb-2"></div>
             <div className="place-self-center">
                 <h1 className=" text-xl text-center "> Our Philosophy </h1>
                 <div className="max-w-lg mx-auto">
-                    <ul>
+                    <ul className="text-center">
                      <li> Provide high quality market research </li>
                      <li> Provide superior client contact & communication </li>
                      <li> Provide detailed analysis of market transactions & trends </li>
