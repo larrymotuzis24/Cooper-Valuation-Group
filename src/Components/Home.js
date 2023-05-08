7cef04ff95b36d97262a9c04d2845efc96500da2
Larry Motuzis <mlauris2006@yahoo.com>
2023-05-08 13:34:48 -0500

    added home page content

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,8 +3,33 @@ import React from "react";
 
 const Home = () => {
     return (
-        <div>
-            <h2 className="text-3xl font-bold"> Home Page </h2>
+        <div className="flex flex-col w-5/6 mx-auto">
+            <h2 className="text-3xl font-bold text-center"> Home Page </h2>
+            <div className="w-36 border border-black place-self-center"></div>
+            <div className="w-5/6 self-center mt-4">
+                <h2 className="text-2xl" > The <span className="font-bold">Cooper Valuation Group</span> is a commercial real estate appraisal and market analysis firm specializing in healthcare and senior housing properties.</h2>
+            </div>
+            <div className="w-3/6 border border-black place-self-center mt-4 mb-2"></div>
+            <div className="place-self-center">
+                <h1 className=" text-xl text-center "> Our Philosophy </h1>
+                <div className="max-w-lg mx-auto">
+                    <ul>
+                     <li> Provide high quality market research </li>
+                     <li> Provide superior client contact & communication </li>
+                     <li> Provide detailed analysis of market transactions & trends </li>
+                    </ul>
+                </div>
+            </div>
+                <div className="w-3/6 border border-black place-self-center mt-4 mb-4"></div>
+            <div>
+                <p className="mb-4"> Our HUD experience includes completed assignments for HUD’s Lean Program for insuring mortgages for Section 232 (nursing and assisted living facilities).
+                     We also have completed assignments for HUD’s MAP lending program for Section 223, 220, and 221.
+                      We are your best choice for Appraisal Reports and Market Studies for healthcare or apartment owners considering HUD-insured financing.</p>
+
+                <p> Our valued clients include HUD Lenders, banks, financial institutions, private firms, developers, owners/operators, and buyers/sellers. Our clients are active in Illinois, Iowa, Wisconsin, Indiana, Ohio, Missouri, and Michigan.</p>
+            </div>
+            
+            
         </div>
     )
 };
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom';
 
 
 const Nav = () => {
-    const properties = "block mt-4 lg:inline-block lg:mt-0 text-black-200 hover:font-bold mr-4"
+    const linkStyles = "block mt-4 lg:inline-block lg:mt-0 text-black-200 hover:font-bold mr-4";
     return (
         <div className="border-b border-black">
-            <Link to='/' className={properties}> Home </Link>
-            <Link to='/about' className={properties}> About Us </Link>
-            <Link to='/services' className={properties}> Products&Services </Link>
-            <Link to='/clients' className={properties}> Clients&Testimonials </Link>
-            <Link to='/newsletters' className={properties}> Newsletters </Link>
-            <Link to='/press' className={properties}> Press </Link>
-            <Link to='/contact' className={properties}> Contact Us </Link>
+            <Link to='/' className={linkStyles}> Home </Link>
+            <Link to='/about' className={linkStyles}> About Us </Link>
+            <Link to='/services' className={linkStyles}> Products&Services </Link>
+            <Link to='/clients' className={linkStyles}> Clients&Testimonials </Link>
+            <Link to='/newsletters' className={linkStyles}> Newsletters </Link>
+            <Link to='/press' className={linkStyles}> Press </Link>
+            <Link to='/contact' className={linkStyles}> Contact Us </Link>
         </div>
     )
 };
