dd643fbd1ba47cc71ecf4b095c5eeb9959a5dea3
Larry Motuzis <mlauris2006@yahoo.com>
2023-05-10 14:08:17 -0500

    updated carousel inerval and images

--- src/Components/CarouselComponent.js
+++ src/Components/CarouselComponent.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import '../carouselStyles.css';
-import image1 from '../carouselImages/HPIM6717.jpg';
-import image2 from '../carouselImages/HPIM6732.jpg';
-import image3 from '../carouselImages/HPIM6736.jpg';
-import image4 from '../carouselImages/HPIM6739.jpg';
-import image5 from '../carouselImages/HPIM6751.jpg';
+import image1 from '../carouselImages/unnamed-1.jpg';
+import image2 from '../carouselImages/unnamed-2.jpg';
+import image3 from '../carouselImages/unnamed-3.jpg';
+import image4 from '../carouselImages/unnamed-4.jpg';
+import image5 from '../carouselImages/unnamed-5.jpg';
+import image6 from '../carouselImages/unnamed.jpg';
 
 const CarouselComponent = () => {
   const images = [
@@ -15,6 +16,7 @@ const CarouselComponent = () => {
     { src: image3, alt: 'Carousel slide 3' },
     { src: image4, alt: 'Carousel slide 4' },
     { src: image5, alt: 'Carousel slide 5' },
+    { src: image6, alt: 'Carousel slide 6' },
   ];
 
   const imageComponents = images.map((image, index) => (
@@ -33,7 +35,7 @@ const CarouselComponent = () => {
         <Carousel
           autoPlay
           infiniteLoop
-          interval={5000}
+          interval={3000}
           transitionTime={1000}
           showThumbs={false}
           showStatus={false}
